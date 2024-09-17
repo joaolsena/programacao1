78e5334e2cbb5c621b4f0c9f41c214d70df3f14b
joaolsena <joaolsena129@gmail.com>
2024-09-16 21:53:26 -0300

    inferno

--- atividade_extra/problema5.c
+++ atividade_extra/problema5.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    
+    int n, retangulos=0,c, v, *vetor= NULL, contador=0;
+    while (1)
+    {
+         scanf("%d", &n);
+        if(n==0)
+        break;
+         vetor=(int*) realloc(vetor, (contador +1) * sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d %d", &c, &v);
+        retangulos+=v/2;   
+    }
+    vetor[contador]= retangulos/2;
+     retangulos=0;
+     contador++;
+    }
+      for (int i = 0; i < contador; i++){
+    printf("%d\n", vetor[i]);
+}
+      free(vetor);
+    return 0; 
 }
