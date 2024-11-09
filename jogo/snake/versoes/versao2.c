56885a65d0fe13362417ef4763de5853504a7bd9
joaolsena <joaolsena129@gmail.com>
2024-11-09 10:46:56 -0300

    a

--- jogo/snake/versoes/versao2.c
+++ jogo/snake/versoes/versao2.c
@@ -5,8 +5,8 @@
 #include <time.h>
 #include <fcntl.h>
 
-#define MAX_tela_X 40
-#define MAX_tela_y 40
+#define MAX_tela_X 30
+#define MAX_tela_y 30
 #define MAX_margem 30
 
 #define forma_cabeca '0'
@@ -16,21 +16,31 @@
 
 #define ATRASO_TIQUE 200000
 
+int ponto=0;
+
 char imagem[MAX_tela_y][MAX_tela_X] = {0};
 
 typedef struct {
     int x;
     int y;
-} passaro;
+} cabecas;
 
-passaro bird;
+cabecas cabeca;
 
 typedef struct {
     int x;
     int y;
-} obstacluos;
+} corpos;
 
-obstacluos obstacluo;
+corpos corpo;
+
+typedef struct {
+    int x;
+    int y;
+    int ativo;
+} comidas;
+
+comidas comida;
 
 int kbhit(void) {
     struct termios oldt, newt;
@@ -74,27 +84,69 @@ void limpar() {
 }
 
 void tela() {
+     printf("%*sPontos: %d\n", MAX_margem + 45, "", ponto);
     for (int i = 0; i < MAX_tela_y; i++) {
         for (int k = 0; k < MAX_margem; k++) {
             printf("  ");
         }
         for (int j = 0; j < MAX_tela_X; j++) {
-            if (i == 0) {
-                printf("\033[37m_\033[0m");
-            } else if (j == 0 || j == MAX_tela_X - 1) {
-                printf("|");
-            } else if (i == MAX_tela_y - 1) {
-                printf("-");
+            if (j == 0 || j == MAX_tela_X - 1 || i == MAX_tela_y - 1 || i == 0) {
+                printf("#");
             } else if (imagem[i][j] == forma_cabeca) {
-                printf("\033[34m%c\033[0m", forma_cabeca);
+                printf("\033[32m%c\033[0m", forma_cabeca);
             } else if (imagem[i][j] == forma_corpo) {
-                printf("%c", forma_corpo);
+                printf("\033[32m%c\033[0m", forma_corpo);
             }else if (imagem[i][j] == forma_comida) {
-                printf("%c", forma_comida);
+                printf("\033[31m%c\033[0m", forma_comida);
                 } else {
                 printf(" ");
             }
         }
         putchar('\n');
     }
+}
+
+void inicia_cobra(){
+    cabeca.x=MAX_tela_X/2;
+    cabeca.y=MAX_tela_y/2;
+    imagem[cabeca.y][cabeca.x]=forma_cabeca;
+}
+
+void inicia_comida(){
+    comida.x= rand() % MAX_tela_X-1;
+    comida.y= rand() % MAX_tela_y-1;
+    comida.ativo=1;
+    imagem[comida.y][comida.x]=forma_comida;
+}
+
+void colisa_comida(){
+    if(comida.ativo && cabeca.x== comida.x && cabeca.y==comida.y){
+        ponto+=10;
+        comida.ativo=0;
+         imagem[comida.y][comida.x]=' ';
+         inicia_comida();        
+    }
+}
+
+void mover_cobra(){
+     imagem[cabeca.y][cabeca.x]=' ';
+ if (kbhit()) {
+     char move = getch();
+     if (move=='a') cabeca.x--;
+     if (move=='d') cabeca.x++;
+     if (move=='w') cabeca.y--;
+     if (move=='s') cabeca.y++; 
+}
+imagem[cabeca.y][cabeca.x]= forma_cabeca;
+}
+int main(){
+    inicia_cobra();
+    inicia_comida();
+    while (1){
+        limpar();
+        tela();
+        colisa_comida();
+        mover_cobra();
+    }
+    
 }
