0c674bf586b25b70a5daa49551efad0ad7eddd11
joaolsena <joaolsena129@gmail.com>
2025-06-01 17:11:11 -0300

    a

---
+++ prog-3/atividade/atividade1.java
@@ -0,0 +1,17 @@
+
+public class atividade1 {
+
+    public static void main(String[] args) {
+        int a= 25000;
+        int b= 68000;
+        int c=0;
+        while (a<b) {
+            a = a+(a*10/100);
+            b = b+(b*2/100);
+            c++;
+            
+        }
+        System.out.println(c);
+    }
+    
+}
---
+++ prog-3/aulas/aula1.java
@@ -0,0 +1,23 @@
+public class aula1 {
+      public static void main(String[] args) {
+    int a=6;
+    float b= 5.7f;
+    long c= 13;
+    String nome= "joao the best";
+    System.out.println("valor de a:"+a+" valor de b: " +c+ " " +b+"  "+nome);
+
+    if(a>10)
+    System.out.println("mair");
+    else if(a==10)
+    System.out.println("mesmo");
+    else
+    System.out.println("menor");
+
+    while (a<=10) {
+        System.out.println("ai calica");
+        a++;
+        
+    }
+      }
+    
+}
